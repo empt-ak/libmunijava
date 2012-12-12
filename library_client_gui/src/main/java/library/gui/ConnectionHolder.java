407876508fc02c8335dc5c707d04e38331cf7ccf
nemusko@gmail.com <nemusko@gmail.com@48a1d447-1c43-4362-3c86-98cce12d9109>
2012-12-12 17:14:17 +0000

    Pridane prihlasovanie do gui, este je nutne poriesit mozne vynimky, zatial ide akakolvek kombinacia hesla a mena

--- library_client_gui/src/main/java/library/gui/ConnectionHolder.java
+++ library_client_gui/src/main/java/library/gui/ConnectionHolder.java
@@ -4,94 +4,90 @@
  */
 package library.gui;
 
-import library.gui.tools.Tools;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import javax.xml.ws.BindingProvider;
+import library.gui.tools.Tools;
 import library.webservice.BookWebService;
 import library.webservice.UserWebService;
 import library.webservice.impl.BookWebServiceImplService;
 import library.webservice.impl.UserWebServiceImplService;
 
-
 /**
  *
  * @author Emptak
  */
-public class ConnectionHolder 
-{
+public class ConnectionHolder {
+
     private BookWebService bws = null;
     private UserWebService uws = null;
     private static final String ERROR = java.util.ResourceBundle.getBundle("Messages").getString("gui.connection.error");
-    
-    
-    public ConnectionHolder() throws ConnectException
-    {
-        if(check())
-        {
-            bws = new BookWebServiceImplService().getBookWebServiceImplPort();
-            uws = new UserWebServiceImplService().getUserWebServiceImplPort();            
-        }
-        else
-        {
-            error();
-            throw new ConnectException(ERROR);
-        }         
+    private static ConnectionHolder instance = new ConnectionHolder();
+
+    public static ConnectionHolder getInstance() {
+        return instance;
     }
 
-    public BookWebService getBws() throws ConnectException
-    {
-        if(!check())
-        {
+    public ConnectionHolder() {
+        
+    }
+
+    public void setServiceCredentials(String userName, String password) throws ConnectException {
+        if (check()) {
+            bws = new BookWebServiceImplService().getBookWebServiceImplPort();
+            uws = new UserWebServiceImplService().getUserWebServiceImplPort();
+            ((BindingProvider) bws).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, userName);
+            ((BindingProvider) bws).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, password);
+            ((BindingProvider) uws).getRequestContext().put(BindingProvider.USERNAME_PROPERTY, userName);
+            ((BindingProvider) uws).getRequestContext().put(BindingProvider.PASSWORD_PROPERTY, password);
+        } else {
             error();
             throw new ConnectException(ERROR);
         }
-        else
-        {
-            if(bws == null)
-            {
+    }
+    
+    public BookWebService getBws() throws ConnectException {
+        if (!check()) {
+            error();
+            throw new ConnectException(ERROR);
+        } else {
+            if (bws == null) {
                 bws = new BookWebServiceImplService().getBookWebServiceImplPort();
             }
             return bws;
-        }        
+        }
     }
 
-    public UserWebService getUws() throws ConnectException
-    {
-        if(!check())
-        {
+    public UserWebService getUws() throws ConnectException {
+        if (!check()) {
             error();
             throw new ConnectException(ERROR);
-        }
-        else
-        {
-            if(uws == null)
-            {
+        } else {
+            if (uws == null) {
                 uws = new UserWebServiceImplService().getUserWebServiceImplPort();
             }
             return uws;
-        }        
+        }
     }
-    
-    
-    private boolean check()
-    {
+
+    private boolean check() {
         return checkIfURLExists("http://localhost:8080/pa165/services/");
     }
-    
+
     /**
-     * Method used for checking if web service on specified port and address is running.
-     * taken from http://singztechmusings.wordpress.com/2011/05/26/java-how-to-check-if-a-web-page-exists-and-is-available/
-     * 
+     * Method used for checking if web service on specified port and address is
+     * running. taken from
+     * http://singztechmusings.wordpress.com/2011/05/26/java-how-to-check-if-a-web-page-exists-and-is-available/
+     *
      * @param targetUrl url to be checked
-     * @return true if server is running and given url is one of ours, false otherwise
+     * @return true if server is running and given url is one of ours, false
+     * otherwise
      */
-    private static boolean checkIfURLExists(String targetUrl) 
-    {
+    private static boolean checkIfURLExists(String targetUrl) {
         HttpURLConnection httpUrlConn = null;
         boolean exists = false;
-        try 
-        {
+        try {
             httpUrlConn = (HttpURLConnection) new URL(targetUrl).openConnection();
 
             httpUrlConn.setRequestMethod("HEAD");
@@ -102,24 +98,20 @@ public class ConnectionHolder
 
             // Print HTTP status code/message for your information.
             System.out.println("Response Code: "
-                                + httpUrlConn.getResponseCode());
+                    + httpUrlConn.getResponseCode());
             System.out.println("Response Message: "
-                                + httpUrlConn.getResponseMessage());
+                    + httpUrlConn.getResponseMessage());
             System.out.println("WEB Service is running");
 
-            exists =  (httpUrlConn.getResponseCode() == HttpURLConnection.HTTP_OK);
-        } 
-        catch (Exception e) 
-        {
-            System.out.println("Error: " + e.getMessage());        
+            exists = (httpUrlConn.getResponseCode() == HttpURLConnection.HTTP_OK);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
-        
+
         return exists;
     }
-    
-    private void error()
-    {
+
+    private void error() {
         Tools.createErrorDialog(ERROR);
     }
-    
 }
--- library_client_gui/src/main/java/library/gui/MainFrame.java
+++ library_client_gui/src/main/java/library/gui/MainFrame.java
@@ -4,6 +4,8 @@
  */
 package library.gui;
 
+import com.jgoodies.looks.plastic.PlasticXPLookAndFeel;
+import java.awt.CardLayout;
 import library.gui.tools.Tools;
 import java.net.ConnectException;
 import library.gui.edit.EditBookDialog;
@@ -44,7 +46,16 @@ public class MainFrame extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
+        jPanel1 = new javax.swing.JPanel();
+        jTextField1 = new javax.swing.JTextField();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanelBooks = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -75,6 +86,64 @@ public class MainFrame extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("Messages"); // NOI18N
         setTitle(bundle.getString("gui.frame.title.main")); // NOI18N
+        getContentPane().setLayout(new java.awt.CardLayout());
+
+        jPanel1.setLayout(new java.awt.GridBagLayout());
+
+        jTextField1.setPreferredSize(new java.awt.Dimension(100, 20));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 53;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel1.add(jTextField1, gridBagConstraints);
+
+        jPasswordField1.setPreferredSize(new java.awt.Dimension(48, 20));
+        jPasswordField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jPasswordField1ActionPerformed(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.ipadx = 105;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel1.add(jPasswordField1, gridBagConstraints);
+
+        jButton1.setText("Login");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel1.add(jButton1, gridBagConstraints);
+
+        jLabel1.setText("Username");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        jPanel1.add(jLabel1, gridBagConstraints);
+
+        jLabel2.setText("Password");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        jPanel1.add(jLabel2, gridBagConstraints);
+
+        jLabel3.setText("gsa");
+        jLabel3.setMaximumSize(new java.awt.Dimension(50, 400));
+        jLabel3.setMinimumSize(new java.awt.Dimension(50, 400));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        jPanel1.add(jLabel3, gridBagConstraints);
+
+        getContentPane().add(jPanel1, "card3");
 
         jTableBooks.setModel(new BookTableModel());
         jScrollPane1.setViewportView(jTableBooks);
@@ -180,7 +249,7 @@ public class MainFrame extends javax.swing.JFrame {
                     .addGroup(jPanelBooksLayout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(jPanelBooksLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 519, Short.MAX_VALUE)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 574, Short.MAX_VALUE)
                             .addGroup(jPanelBooksLayout.createSequentialGroup()
                                 .addComponent(jButtonResetBooksTable, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 0, Short.MAX_VALUE)))))
@@ -211,7 +280,7 @@ public class MainFrame extends javax.swing.JFrame {
                         .addGroup(jPanelBooksLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabelFilterByDepartment))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 70, Short.MAX_VALUE)
                 .addComponent(jButtonResetBooksTable)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 414, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -267,7 +336,7 @@ public class MainFrame extends javax.swing.JFrame {
             .addGroup(jPanelUsersLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanelUsersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 519, Short.MAX_VALUE)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 574, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelUsersLayout.createSequentialGroup()
                         .addGroup(jPanelUsersLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanelUsersLayout.createSequentialGroup()
@@ -300,25 +369,30 @@ public class MainFrame extends javax.swing.JFrame {
                     .addComponent(jButtonUserTableRefresh)
                     .addComponent(jButtonDeleteUser))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 462, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 504, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
         jTabbedPane1.addTab(bundle.getString("gui.frame.tabbed.users"), jPanelUsers); // NOI18N
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jTabbedPane1)
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 599, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(jTabbedPane1))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jTabbedPane1)
-                .addContainerGap())
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 655, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                    .addComponent(jTabbedPane1)
+                    .addContainerGap()))
         );
+
+        getContentPane().add(jPanel2, "card2");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -524,6 +598,14 @@ public class MainFrame extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButtonUserSearchActionPerformed
 
+    private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jPasswordField1ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        initAuth();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -540,7 +622,7 @@ public class MainFrame extends javax.swing.JFrame {
             }
             else
             {
-                javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+                javax.swing.UIManager.setLookAndFeel(new PlasticXPLookAndFeel()); 
             }
             
 //            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -563,6 +645,7 @@ public class MainFrame extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButtonCreateBook;
     private javax.swing.JButton jButtonCreateUser;
     private javax.swing.JButton jButtonDeleteBook;
@@ -575,17 +658,24 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JButton jButtonUserSearch;
     private javax.swing.JButton jButtonUserTableRefresh;
     private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabelFilterByDepartment;
     private javax.swing.JLabel jLabelRealName;
     private javax.swing.JLabel jLabelSearchByAuthor;
     private javax.swing.JLabel jLabelSearchByTitle;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanelBooks;
     private javax.swing.JPanel jPanelUsers;
+    private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTable jTableBooks;
     private javax.swing.JTable jTableUsers;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextFieldBookAuthor;
     private javax.swing.JTextField jTextFieldBookTitle;
     private javax.swing.JTextField jTextFieldSearchByRealName;
@@ -603,11 +693,19 @@ public class MainFrame extends javax.swing.JFrame {
     
     private void myInit()
     {
+        setLocationRelativeTo(null);
+    }
+    
+    private void initAuth() {
         boolean connSucc = true;
+        
+        String userName = jTextField1.getText();
+        String password = new String(jPasswordField1.getPassword());
         
         try
         {
-            conn = new ConnectionHolder();
+            conn = ConnectionHolder.getInstance();
+            conn.setServiceCredentials(userName,password);
         }
         catch(ConnectException ex)
         {
@@ -619,13 +717,15 @@ public class MainFrame extends javax.swing.JFrame {
             try
             {
                 getBTM().addBooks(conn.getBws().getAllBooks());
-                getUTM().addUsers(conn.getUws().getUsers());            
+                getUTM().addUsers(conn.getUws().getUsers());  
+                CardLayout cl = (CardLayout) getContentPane().getLayout();
+                cl.show(getContentPane(), "card2");
             }
             catch(ConnectException | NullPointerException ex)
             {
                 System.err.println(ex.getMessage());
             }            
         }
-        
     }
+
 }
