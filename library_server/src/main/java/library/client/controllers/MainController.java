cd27a5075731ea4bca90d563997d6f03836e7b41
nemusko@gmail.com <nemusko@gmail.com@48a1d447-1c43-4362-3c86-98cce12d9109>
2013-01-17 19:35:57 +0000

    Odstraneny installer

--- library_server/src/main/java/library/client/controllers/MainController.java
+++ library_server/src/main/java/library/client/controllers/MainController.java
@@ -4,10 +4,6 @@
  */
 package library.client.controllers;
 
-import javax.annotation.PostConstruct;
-import library.client.Installer;
-import library.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,24 +15,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/")
 public class MainController
-{
-    @Autowired
-    private Installer installer;
-    
+{    
     @RequestMapping("/")
     public ModelAndView show() throws Exception 
     {
         return new ModelAndView("index");
     }
 
-    /**
-     * Method used for setting up database. It uses @PostConstruct annotation which is registered to spring
-     * and after initializing entire bean all methods with this annotation are called. This comes handy when we
-     * run application for the first run since we use in-memory database that is cleaned with each start of 
-     * application server.
-     */
-    @PostConstruct
-    public void install(){
-        installer.firstRun();
-    }
 }
