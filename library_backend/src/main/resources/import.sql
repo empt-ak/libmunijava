d6567e65f7710bf22bcfe74dcc8da929813cb498
nemusko@gmail.com <nemusko@gmail.com@48a1d447-1c43-4362-3c86-98cce12d9109>
2013-01-17 19:37:11 +0000

    Updatovany import.sql

--- library_backend/src/main/resources/import.sql
+++ library_backend/src/main/resources/import.sql
@@ -3,3 +3,16 @@ insert into Users (username,password,realName,systemRole) values ('andrej', '011
 insert into Users (username,password,realName,systemRole) values ('nemo', 'e81498977973fe4eef602ef6567141c08f2a1e3f', 'Peter Nemček', 'ADMINISTRATOR');
 insert into Users (username,password,realName,systemRole) values ('admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Admin Adminovič Adminov', 'ADMINISTRATOR');
 insert into Users (username,password,realName,systemRole) values ('user', '12dea96fec20593566ab75692c9949596833adc9', 'User Userovič Sergejev', 'USER');
+
+insert into Book (title,author,department,bookStatus) values ('Emma','Jane Austen','ADULT','AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('Pride & Prejudice', 'Jane Austen', 'ADULT', 'AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('Sense and Sensibility', 'Jane Austen', 'ADULT', 'AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('Harry Potter and the Philosophers Stone', 'J.K. Rowling', 'ADULT', 'AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 'ADULT', 'AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('Harry Potter and the Prisoner of Azkaban', 'J.K. Rowling', 'ADULT', 'AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('On the Generalized Theory of Gravitation', 'Albert Einstein', 'SCIENTIFIC', 'AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('čerešnička', 'ľščťžýáíéúäň', 'ADULT', 'AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('The Grand Design', 'Stephen Hawking', 'SCIENTIFIC', 'AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('Spring Security 3', 'Peter Mularien', 'SCIENTIFIC', 'AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('Tri gaštanové kone', 'Margita Figuli', 'ADULT', 'AVAILABLE');
+insert into Book (title,author,department,bookStatus) values ('Spring Security', 'Peter Mularien', 'SCIENTIFIC', 'AVAILABLE'); 
